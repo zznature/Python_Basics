2188c69c4eb97bab2ad7c04851b23191c67a9f7a
ZHOU Zhang <zhouzhang92@gmail.com>
2022-05-27 19:48:13 +0800

    re-organize files

--- Python-Concepts/Examples/Exception_handling.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        n = input("Please enter an integer: ")
-        n = int(n)
-        break
-    except ValueError:
-        print("No valid integer! Please try again ...")
-
-print("Great, you successfully entered an integer!")
