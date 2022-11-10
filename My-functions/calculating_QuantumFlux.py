f500ee59f4692b7a55e91de860ef5304dd161c97
ZHOU Zhang <zhouzhang92@gmail.com>
2022-11-10 20:56:34 +0800

    11.10 update

--- My-functions/calculating_QuantumFlux.py
+++ My-functions/calculating_QuantumFlux.py
@@ -3,7 +3,7 @@
 import scipy.constants as cons
 
 a = 0.246e-9 #unit: m
-theta = 1.47/180*3.14159
+theta = 1.44/180*3.14159
 S = 1.732/2*(a/theta)**2
 quantum_flux = cons.h/cons.e/S  # unit in Tesla
 
