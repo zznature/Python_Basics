015fbcdb5d3520985a46405881e51e196bbab827
ZHOU Zhang <zhouzhang92@gmail.com>
2023-08-02 22:59:12 +0800

    move deep-learing to another deposi

--- Deep-Learning/OriginalCodes/zztestNetwork.py
+++
@@ -1,6 +0,0 @@
-import network
-import mnist_loader
-
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 25, 10, 0.1, test_data=test_data) 
